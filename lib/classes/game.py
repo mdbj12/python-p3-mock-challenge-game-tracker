7ad531a08f6b94f082061796336af20406cd6cef
Michael Jeung <michaelj229@gmail.com>
2023-05-10 15:18:27 -0400

    done

--- lib/classes/game.py
+++ lib/classes/game.py
@@ -4,13 +4,31 @@ class Game:
         self._results = []
         self._players = []
         
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if type(title) == str and not hasattr(self, 'title') and len(title) >= 1:
+            self._title = title
+        else:
+            raise Exception
+
     def results(self, new_result=None):
         from classes.result import Result
-        pass
+        if new_result and isinstance(new_result, Result):
+            self._results.append(new_result)
+        return self._results
     
     def players(self, new_player=None):
         from classes.player import Player
-        pass
+        if new_player and isinstance(new_player, Player):
+            self._players.append(new_player)
+        return self._players
     
     def average_score(self, player):
-        pass
+        sum = 0 
+        for result in self._results:
+            sum += result.score
+        return sum/len(self._players)
--- lib/classes/player.py
+++ lib/classes/player.py
@@ -6,20 +6,39 @@ class Player:
         self.username = username
         self._results = []
         self._games_played = []
+        Player.all.append(self)
+
+    @property
+    def username(self):
+        return self._username
+    
+    @username.setter
+    def username(self, username):
+        if isinstance(username, str) and 2 <= len(username) <= 16:
+            self._username = username
+        else:
+            raise Exception
         
     def results(self, new_result=None):
         from classes.result import Result
-        pass
+        if new_result and isinstance(new_result, Result):
+            self._results.append(new_result)
+        return self._results
     
     def games_played(self, new_game=None):
         from classes.game import Game
-        pass
+        if new_game and isinstance(new_game, Game):
+            self._games_played.append(new_game)
+        return self._games_played
     
     def played_game(self, game):
-        pass
+        if game in self._games_played:
+            return True
+        else:
+            return False
     
     def num_times_played(self, game):
-        pass
+        return len([r for r in self._results if r.game == game])
     
     @classmethod
     def highest_scored(cls, game):
